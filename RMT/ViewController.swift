3a373be532b6b81c126b1a76f1e28526363d9e91
goodvt9 <goodvt9@gmail.com>
2023-08-05 09:12:38 +0800

    Initial Commit

---
+++ RMT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RMT
+//
+//  Created by vt9 on 2023/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
